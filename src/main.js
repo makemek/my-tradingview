155c47f8b590ca3463c073a2c63143ed7e13ff0d
Apipol Niyomsak <apipol.niyomsak@kingpower.com>
2019-05-05 16:51:25 +0700

    refactor message handler

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
+import debug from 'debug'
 import SocketMessageHooker from './SocketMessageHooker'
 import { ioFilter } from 'modules/common/helpers'
-import debug from 'debug'
-import { Builder } from 'modules/message/protobuf'
-import { compose, decompose } from 'modules/message/payload'
-import { isHeartbeat } from 'modules/message/heartbeat'
-import { Buffer } from 'buffer'
+import {
+  handleStringMessage,
+  handleBufferMessage,
+} from 'modules/message/message-handler'
 
 const log = debug(`${process.env.APP_NAME}:main`)
 
@@ -46,55 +46,11 @@ function injectWebSocket() {
   }
 }
 
-function handleStringMessage(message) {
-  const messages = decompose.withStringPayload(message)
-  const [firstMessage] = messages
-  const heartbeatSignature = firstMessage.payload.slice(0, 4)
-  if (messages.length === 1 && isHeartbeat(heartbeatSignature)) {
-    return ioFilter.apply('heartbeat', heartbeatSignature)
-  }
-  const modifiedMessages = messages.map((message) => {
-    const { payload } = message
-    const { m: command, p: data } = JSON.parse(payload)
-    const modifiedData = ioFilter.apply(command, data)
-    const inputPayload = JSON.stringify({
-      m: command,
-      p: modifiedData,
-    })
-    log('decomposed', command, data)
-    return compose.withStringPayload(inputPayload)
-  })
-
-  return modifiedMessages.join('')
-}
-
-function handleBufferMessage(message, builder) {
-  const messages = decompose.withBufferPayload(message)
-  const [firstMessage] = messages
-  const heartbeatSignature = Buffer.from(
-    firstMessage.payload.slice(0, 4),
-  ).toString()
-  if (messages.length === 1 && isHeartbeat(heartbeatSignature)) {
-    return ioFilter.apply('heartbeat', heartbeatSignature)
-  }
-  const modifiedMessages = messages.map((message) => {
-    const { payload } = message
-    const { command, data } = builder.decode(payload)
-    const modifiedData = ioFilter.apply(command, data)
-    const inputPayload = builder.encode(command, modifiedData)
-    log('decomposed', command, data)
-    return compose.withBufferPayload(inputPayload)
-  })
-
-  return Buffer.concat(modifiedMessages)
-}
-
 function makeFilter() {
-  const builder = new Builder()
   const messageHandler = (message) =>
     typeof message === 'string'
       ? handleStringMessage(message)
-      : handleBufferMessage(message, builder)
+      : handleBufferMessage(message)
   ioFilter.bind(SocketMessageHooker.SOCKET_RECEIVE, messageHandler)
   ioFilter.bind(SocketMessageHooker.SOCKET_SEND, messageHandler)
 
---
+++ src/modules/message/message-handler.js
@@ -0,0 +1,52 @@
+import debug from 'debug'
+import { Buffer } from 'buffer'
+import { ioFilter } from 'modules/common/helpers'
+import { compose, decompose } from './payload'
+import { isHeartbeat } from './heartbeat'
+import { Builder } from './protobuf'
+
+const log = debug(`${process.env.APP_NAME}:message-handler`)
+const builder = new Builder()
+
+export function handleStringMessage(message) {
+  const messages = decompose.withStringPayload(message)
+  const [firstMessage] = messages
+  const heartbeatSignature = firstMessage.payload.slice(0, 4)
+  if (messages.length === 1 && isHeartbeat(heartbeatSignature)) {
+    return ioFilter.apply('heartbeat', heartbeatSignature)
+  }
+  const modifiedMessages = messages.map((message) => {
+    const { payload } = message
+    const { m: command, p: data } = JSON.parse(payload)
+    const modifiedData = ioFilter.apply(command, data)
+    const inputPayload = JSON.stringify({
+      m: command,
+      p: modifiedData,
+    })
+    log('decomposed', command, data)
+    return compose.withStringPayload(inputPayload)
+  })
+
+  return modifiedMessages.join('')
+}
+
+export function handleBufferMessage(message) {
+  const messages = decompose.withBufferPayload(message)
+  const [firstMessage] = messages
+  const heartbeatSignature = Buffer.from(
+    firstMessage.payload.slice(0, 4),
+  ).toString()
+  if (messages.length === 1 && isHeartbeat(heartbeatSignature)) {
+    return ioFilter.apply('heartbeat', heartbeatSignature)
+  }
+  const modifiedMessages = messages.map((message) => {
+    const { payload } = message
+    const { command, data } = builder.decode(payload)
+    const modifiedData = ioFilter.apply(command, data)
+    const inputPayload = builder.encode(command, modifiedData)
+    log('decomposed', command, data)
+    return compose.withBufferPayload(inputPayload)
+  })
+
+  return Buffer.concat(modifiedMessages)
+}
