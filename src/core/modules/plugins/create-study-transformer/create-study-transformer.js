07a74b3468fd728d9464de7225e180fdd7e469b7
Apipol Niyomsak <apipol.niyomsak@kingpower.com>
2020-07-06 21:42:44 +0700

    plugin use rxjs implementation

--- src/core/modules/plugins/create-study-transformer/create-study-transformer.js
+++ src/core/modules/plugins/create-study-transformer/create-study-transformer.js
@@ -6,6 +6,7 @@
  */
 
 import { Buffer } from 'buffer'
+import { map } from 'rxjs/operators'
 //--
 import { logger } from 'lib/logger'
 
@@ -14,34 +15,40 @@ const log = logger('core:plugins:create-study-transformer')
 export default () => (hook) => {
   const { create_study } = hook.EVENT
 
-  hook.bind(create_study, (data) => {
-    // the 'inputs' have the same format as 'create_study' protobuf implicitly
-    const { inputs } = data
-    const createStudyModel = {
-      session: undefined,
-      study_name: undefined,
-      turnaround: undefined,
-      parent: undefined,
-      study: undefined,
-      inputs: undefined,
-    }
+  hook.bind(create_study, _handleCreateStudy)
+}
 
-    if (inputs.buffer) {
-      const inputString = Buffer.from(inputs.buffer).toString()
+export function _handleCreateStudy(stream$) {
+  return stream$.pipe(
+    map((data) => {
+      // the 'inputs' have the same format as 'create_study' protobuf implicitly
+      const { inputs } = data
+      const createStudyModel = {
+        session: undefined,
+        study_name: undefined,
+        turnaround: undefined,
+        parent: undefined,
+        study: undefined,
+        inputs: undefined,
+      }
 
+      if (inputs.buffer) {
+        const inputString = Buffer.from(inputs.buffer).toString()
+
+        data.$inputsDecoded = {
+          ...createStudyModel,
+          ...tokenize(inputString),
+        }
+        log('transform', inputString, data.$inputsDecoded)
+        return data
+      }
       data.$inputsDecoded = {
         ...createStudyModel,
-        ...tokenize(inputString),
+        inputs: data.inputs,
       }
-      log('transform', inputString, data.$inputsDecoded)
       return data
-    }
-    data.$inputsDecoded = {
-      ...createStudyModel,
-      inputs: data.inputs,
-    }
-    return data
-  })
+    }),
+  )
 }
 
 function tokenize(str) {
--- src/interceptor/example/example.js
+++ src/interceptor/example/example.js
@@ -1,18 +1,27 @@
+/**
+ * An example demonstrates how to implement a plugin which listens to events
+ * and do something with it.
+ * A plugin is a custom rxjs operator
+ */
+
 import { logger } from 'lib/logger'
+import { tap } from 'rxjs/operators'
 
 const log = logger('interceptor:example')
 
+/**
+ * [description]
+ * @param  {[any]} config plugin's configuration
+ * @return {[Filter]}
+ */
 export default (config) => (hook) => {
   // do anything with the config
 
   // bind to event
   // for ease of use, there is an enum EVENT which contains all available event name
+  // For available event, please take a look at src/modules/common/schema/schema.js
   const { qsd } = hook.EVENT
-  hook.bind(qsd, (data) => {
-    log('processing qsd', data, config)
-
-    // it is a good practice to return filtered data even though we don't do anything to it
-    // by default, if nothing is returned, the original data will be passed to the next listener
-    return data
-  })
+  hook.bind(qsd, (stream$) =>
+    stream$.pipe(tap((data) => log('processing qsd', data, config))),
+  )
 }
